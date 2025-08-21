e9a54b8e57cc785c084497e9fcc6546786e6291d
m9ts <mat3us.gois@gmail.com>
2025-08-21 20:07:31 -0300

    Realizando testes e exercícios

---
+++ Aula01_Introdução/Exercícios/Ex1.java
@@ -0,0 +1,14 @@
+/*
+Edite o programa alterando 32 para 356. Compile e execute. Repita o procedimento
+agora com 35000. Altere novamente o programa modificando o tipo de valor para
+int. Compile e execute. Repita o procedimento alterando o tipo de valor para double.
+Substitua também 35000 por um valor fora dos limites de double (voce pode usar a
+notação científica para isso). O que acontece?
+*/
+
+public class Ex1 {
+    public static void main (String[] args) {
+        short valor = 32;
+        System.out.println(valor + " é um short!");
+    }
+}
---
+++ PrecisaoByte.java
@@ -0,0 +1,8 @@
+public class PrecisaoByte {
+    public static void main(String[] args){
+        byte i = 127;
+        System.out.println("i: " + i);
+        i++;
+        System.out.println("i: " + i);
+    }
+}
