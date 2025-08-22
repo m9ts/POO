5e8d2bb24e66793555c9e4fbbf5c657ada3562cd
m9ts <mat3us.gois@gmail.com>
2025-08-22 12:28:47 -0300

    Finalizado exercício 3 da Revisão - Verifica

--- Aula01_Introdução/Revisão/Verifica.java
+++ Aula01_Introdução/Revisão/Verifica.java
@@ -1,7 +1,30 @@
 /*
-Faça um programa em Java que receba um número inteiro entre 0
-e 9 via args[0] e exiba a palavra correspondente ao número.
+Faça um programa em Java que receba um caractere via args[0] e
+verifique se ele é uma letra maiúscula ou minúscula. Além disso,
+exiba a letra convertida (maiúscula em minúscula, ou vice-versa).
 Exemplo de execução:
-• java Ex1 3
-• Saída: três
+• java Ex3 a
+• Saída: minúscula -> A
 */
+
+public class Verifica {
+    public static void main (String[] args) {
+        if (args.length == 0) {
+            System.out.println("Digite um caractere.");
+            return;
+        }
+        
+        char c = args[0].charAt(0);
+
+        if (!Character.isLetter(c)) {
+            System.out.println("Não é uma letra! Tente digitar uma letra do alfabeto.");
+            return;
+        }
+
+        if (Character.isUpperCase(c)) {
+            System.out.println("Maiúscula para minúscula -> " + Character.toLowerCase(c));
+        } else {
+            System.out.println("Minúscula para maiúscula -> " + Character.toUpperCase(c));
+        }
+    }
+}
