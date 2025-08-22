7a8c5147d05bee68294d61272dce8963c115aa4f
m9ts <mat3us.gois@gmail.com>
2025-08-22 12:07:36 -0300

    Finalizado exercício 2 da Revisão - ClassificaNota

--- Aula01_Introdução/Revisão/ClassificaNota.java
+++ Aula01_Introdução/Revisão/ClassificaNota.java
@@ -14,8 +14,27 @@ Exemplo de execução:
 public class ClassificaNota {
     public static void main (String[] args) {
         if (args.length == 0) {
-            System.out.println("Insira um número entre 0 e 100.");
+            System.out.println("Insira uma nota entre 0 e 100.");
             return;
+        }
+
+        int nota = Integer.parseInt(args[0]);
+
+        if (nota < 0 || nota > 100) {
+            System.out.println("Nota deve estar entre 0 e 100!");
+            return;
+        }
+
+        if (nota >= 90) {
+            System.out.println("A");
+        } else if (nota >= 80) {
+            System.out.println("B");
+        } else if (nota >= 70) {
+            System.out.println("C");
+        } else if (nota >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
         }
 
     }
