7f2c805f79f4da451c1f8ad50c7163d1285fd869
m9ts <mat3us.gois@gmail.com>
2025-08-28 20:01:38 -0300

    Desvios rotulados

---
+++ Aula03_Estruturas/breakRotulado.java
@@ -0,0 +1,47 @@
+public class breakRotulado {
+    public static void main(String[] args) {
+        int i = 0;
+
+        externo: for (; true;) { // laço externo infinito
+            interno: for (; i < 10; i++) {
+                System.out.println("i = " + i);
+
+                if (i == 2) {
+                    prt("continue"); // chamada ao método prt
+                    continue; // continua o loop interno
+                }
+
+                if (i == 3) {
+                    System.out.println("break"); // quebra o loop interno
+                    i++;
+                    break;
+                }
+
+                if (i == 7) {
+                    System.out.println("continue externo"); // continua o loop externo
+                    i++;
+                    continue externo;
+                }
+
+                if (i == 8) {
+                    System.out.println("break externo"); // quebra o loop externo
+                    break externo;
+                }
+
+                // loop interno adicional
+                for (int k = 0; k < 5; k++) {
+                    if (k == 3) {
+                        System.out.println("continue interno");
+                        continue interno;
+                    }
+                }
+            }
+        }
+
+        // Não é possível usar break ou continue com rótulos fora de loops rotulados
+    }
+
+    static void prt(String s) {
+        System.out.println(s);
+    }
+}
