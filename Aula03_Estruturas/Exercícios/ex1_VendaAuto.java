d2bc2dafac5695da5a56ac3effb3c62ecf91ee8c
m9ts <mat3us.gois@gmail.com>
2025-08-28 21:42:55 -0300

    Testando MANIFEST.MF e finalização do ex2

---
+++ Aula03_Estruturas/Exercícios/ex1_VendaAuto.java
@@ -0,0 +1,20 @@
+/*
+Exercício 1: Venda automóvel
+Faça um programa em Java que use as características do comando switch
+para calcular o preço de um automóvel com diversos tipos de opcionais. Considere que o automóvel é vendido em cinco categorias distintas (numeradas
+de 1 a 5), onde cada categoria inclui todos os opcionais da categoria imediatamente inferior (a de número menor).
+A entrada do programa corresponde à categoria do automóvel cujo preço será calculado.
+O programa deve ser implementado de tal maneira que, caso seja necessário
+alterar o preço dos opcionais de uma categoria, a mudança seja feita apenas
+no trecho de código daquela categoria. As categorias superiores (com número maior) não devem precisar de alterações, pois já incluem automaticamente
+os valores acumulados das categorias inferiores
+ */
+
+ package Exercícios;
+
+ public class ex1_VendaAuto {
+     public static void main(String[] args) {
+         
+     }
+ }
+ 
---
+++ Aula03_Estruturas/Exercícios/ex2_ParOuImpar.java
@@ -0,0 +1,31 @@
+/*
+Exercício 2: Par ou ímpar
+Escreva um programa que leia um valor inteiro do usuário e informe se o
+número é par ou ímpar.
+Exemplo de saída:
+Digite um número: 7
+O número 7 é ímpar.
+
+ */
+package Exercícios;
+import java.util.Scanner;
+
+
+public class ex2_ParOuImpar {
+    public ex2_ParOuImpar() {
+    }
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       System.err.println("Digite um número: ");
+      
+       int numero = scanner.nextInt(); 
+       if (numero % 2 == 0) {
+         System.out.println("O número " + numero + " é par.");
+       } else {
+         System.out.println("O número " + numero + " é ímpar.");
+       }
+       
+       scanner.close();
+       
+    }
+ }
