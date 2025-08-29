f7eaa2b5c879e725412b096c43a16029c6bba1b8
m9ts <mat3us.gois@gmail.com>
2025-08-28 21:10:14 -0300

    Atualização na classe Scanner

--- Aula03_Estruturas/Conteúdo/testeScanner.java
+++ Aula03_Estruturas/Conteúdo/testeScanner.java
@@ -1,5 +1,5 @@
+package Conteúdo;
 import java.util.Scanner;
-
 // Programa que lê números inteiros digitados pelo usuário e imprime até que o número 0 seja digitado
 public class testeScanner {
     public static void main (String[] args) {
