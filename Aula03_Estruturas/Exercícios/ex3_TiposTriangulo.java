3fea8b6b9fddeaa048ce310f79d9f51b43ae87aa
m9ts <mat3us.gois@gmail.com>
2025-08-28 22:17:58 -0300

    Inicialização do exercício 3

---
+++ Aula03_Estruturas/Exercícios/ex3_TiposTriangulo.java
@@ -0,0 +1,36 @@
+/*
+Exercício 3: Tipos de Triângulo
+
+Leia três números inteiros a, b e c, representando os lados de um triângulo. Verifique se eles podem formar um triângulo e, caso positivo, indique se o triângulo é equilátero, isósceles ou escaleno.
+
+Condição para formar um triângulo:
+
+a + b > c e a + c > b e b + c > a
+
+Classificação:
+
+Equilátero: a = b = c
+
+Isósceles: a = b ou a = c ou b = c
+
+Escaleno: a ≠ b ≠ c
+
+Exemplo de saída:
+Digite os lados do triângulo: 5 5 8
+Os lados formam um triângulo.
+Tipo: Isósceles
+*/
+
+package Exercícios;
+import java.util.Scanner;
+
+public class ex3_TiposTriangulo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.err.println("Insira três valores: ");
+
+
+
+        scanner.close();
+    }
+}
