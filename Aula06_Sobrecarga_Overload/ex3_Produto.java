86e941719025ecff0ffe120658fafd635baaf270
m9ts <mat3us.gois@gmail.com>
2025-09-18 20:33:46 -0300

    Ex.3 finalizado

--- Aula06_Sobrecarga_Overload/ex3_Produto.java
+++ Aula06_Sobrecarga_Overload/ex3_Produto.java
@@ -20,13 +20,20 @@ public class ex3_Produto {
     }
 
     public void exibirInfo() {
-        System.out.println("Nome: ");
-        System.out.println("Preço: ");
-        System.out.println("Quantidade: ");
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: " + preco);
+        System.out.println("Quantidade: " + quantidade);
+        System.out.println("-----------");
     }
 
     public static void main(String[] args) {
         ex3_Produto prod1 = new ex3_Produto();
         prod1.exibirInfo();
+
+        ex3_Produto prod2 = new ex3_Produto("Detergente", 7.00);
+        prod2.exibirInfo();
+
+        ex3_Produto prod3 = new ex3_Produto("Amaciante", 20.00, 1);
+        prod3.exibirInfo();
     }
 }
