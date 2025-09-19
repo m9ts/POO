0cd131c30c81d5fdc28136ea2cf4bb985b9fcd02
m9ts <mat3us.gois@gmail.com>
2025-09-19 09:52:47 -0300

    Adicionado enunciado do exercício 1

--- Aula06_Sobrecarga_Overload/ex1_Geometria.java
+++ Aula06_Sobrecarga_Overload/ex1_Geometria.java
@@ -1,3 +1,14 @@
+/*
+Crie uma classe Geometria com métodos area
+sobrecarregados:
+1 area(double lado) -> área de um quadrado.
+2 area(double base, double altura) -> área de um
+retângulo.
+3 area(double raio, boolean circulo) -> área de um
+círculo, quando circulo=true.
+• No main, teste cada método e exiba os resultados.
+ */
+
 public class ex1_Geometria {
     public double area(double lado) {
         return lado * lado;
@@ -20,6 +31,6 @@ public class ex1_Geometria {
         ex1_Geometria g = new ex1_Geometria();
         System.out.println(g.area(5));
         System.out.println(g.area(4, 3));
-        System.out.println(g.area(3, true));
+        System.out.println(g.area(6, true));
     }
 }
